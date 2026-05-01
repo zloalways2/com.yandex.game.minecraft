05372edf1d06c33908f9b6123dcbe78ca5926f9b
mastersuperman <smallgamescompany@gmail.com>
2026-05-01 23:27:11 +0300

    init

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "DefaultCompany-minecraft_clone-0.1.0";
+const contentToCache = [
+    "Build/com.yandex.game.minecraft.loader.js",
+    "Build/com.yandex.game.minecraft.framework.js",
+    "Build/com.yandex.game.minecraft.data",
+    "Build/com.yandex.game.minecraft.wasm",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
