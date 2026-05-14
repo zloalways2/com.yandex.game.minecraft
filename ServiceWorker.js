60222a87672137b0acd249affd08c6b08b7d3ca0
mastersuperman <smallgamescompany@gmail.com>
2026-05-15 01:20:58 +0300

    init

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-minecraft_clone-0.1.0";
 const contentToCache = [
-    "Build/265fb8c49b8a64d9b1c57924c92265c3.loader.js",
+    "Build/15f0e5c4173b869da505231974e6a8b7.loader.js",
     "Build/63a39eee47a1cfebb9c9ca80386888f8.framework.js",
-    "Build/066cdb7c223700ca8a4c6b761cc446ad.data",
-    "Build/ef468134ab434c8d81cee29301e68d12.wasm",
+    "Build/d2169c423539f01f081b1c73c0564857.data",
+    "Build/d74ad8ba696745a28ab804048e9d276d.wasm",
     "TemplateData/style.css"
 
 ];
